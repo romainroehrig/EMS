a11c594d065e660c2f2ff48e2955d29dc0f19814
Romain Roehrig <romain.roehrig@gmail.com>
2018-06-11 16:36:08 +0200

    Remove CNRM specificities in macRR branch

---
+++ Examples/config/config_arp631_CMIP6.py
@@ -0,0 +1,11 @@
+import os
+
+rep0 = os.getenv('REP_MUSC')
+
+config = 'CMIP6'
+cycle = 'arp631'
+MASTER = '/Users/romainroehrig/rootpack/arp603_export.01.MPIGNU640.x/bin//MASTERODB'
+PGD = '/Users/romainroehrig/rootpack/arp603_export.01.MPIGNU640.x/bin/PGD'
+PREP = '/Users/romainroehrig/rootpack/arp603_export.01.MPIGNU640.x/bin/PREP'
+namATMref = rep0 + '/namelist/ARPCLIMAT/nam.atm.tl127l91r.CMIP6.v631'
+namSFXref = rep0 + '/namelist/SURFEX/nam.sfx.tl127.CMIP6.v631'
