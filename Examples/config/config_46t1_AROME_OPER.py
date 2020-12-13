abbcb242ae2d6034107bc3c77d1c97baa3434f27
Romain Roehrig (CNRM) <romain.roehrig@gmail.com>
2020-12-13 19:11:01 +0100

    Bugfix for AROME 46t1

--- Examples/config/config_46t1_AROME_OPER.py
+++ Examples/config/config_46t1_AROME_OPER.py
@@ -1,8 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+
 import os
+REP_EMS = os.getenv('REP_EMS')
+REP_MUSC = os.getenv('REP_MUSC')
 
-rep0 = os.getenv('REP_MUSC')
+############# Begin editing
 
-config = 'AROME_OPER'
-cycle = '46_t1.01.GFORTRAN610ECCOD.xfftw'
+GROUP = '46t1'
+# EXPID will be taken from this file name following the convention config_EXPID.py (__name__ = config_EXPID) 
+EXPID = __name__[7:]
+
+# Binaries
 MASTER = '/cnrm/amacs/USERS/roehrig/share/MUSC/46_t1.01.GFORTRAN610ECCOD.xfftw/bin/MASTERODB'
-namATMref = rep0 + '/namelist/AROME/namarp_46t1_AROME_OPER'
+
+# Namelists
+ATMNAM = os.path.join(REP_MUSC, 'namelist/AROME/namarp_46t1_AROME_OPER')
+
+# Model configuration
+vert_grid = os.path.join(REP_MUSC, 'grid/L60_AROME.dta')
+timestep = 50
+
+# Postprocessing
+dirpost = os.path.join(REP_MUSC,'post')
+variablesDict = 'variables.py'
+defaultConfigPost = 'config_default.py'
+caseDependent = True
+lfaformat = 8
+
+# EMS configuration
+model = 'AROME46t1'
+lsurfex = False
+loverwrite = True
+lupdate_ATM = True 
+lupdate_RUN = True
+
+# ecoclimap data
+ecoclimap = os.path.join(REP_EMS, 'UTIL', 'ecoclimap_cnrm_cm6.02')
+
+############# End editing
