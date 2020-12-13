5580a1ddf41c51ccbd541a4e4ee2039771dd20a9
Romain Roehrig <romain.roehrig@gmail.com>
2020-12-13 19:20:30 +0100

    Convergence with macRR_dephy2

--- Examples/config/config_41t1_AROME_OPER.py
+++ Examples/config/config_41t1_AROME_OPER.py
@@ -1,8 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+
 import os
+REP_EMS = os.getenv('REP_EMS')
+REP_MUSC = os.getenv('REP_MUSC')
 
-rep0 = os.getenv('REP_MUSC')
+############# Begin editing
 
-config = 'AROME_OPER'
-cycle = '41t1_op1.11.GFORTRAN610.x.EB.MUSC'
+GROUP = '41t1'
+# EXPID will be taken from this file name following the convention config_EXPID.py (__name__ = config_EXPID) 
+EXPID = __name__[7:]
+
+# Binaries
 MASTER = '/cnrm/amacs/USERS/roehrig/share/MUSC/41t1_op1.11.GFORTRAN610.x.EB.MUSC/bin/MASTERODB'
-namATMref = rep0 + '/namelist/AROME/namarp_41t1_AROME_OPER'
+
+# Namelists
+ATMNAM = os.path.join(REP_MUSC, 'namelist/AROME/namarp_41t1_AROME_OPER')
+
+# Model configuration
+vert_grid = os.path.join(REP_MUSC, 'grid/L60_AROME.dta')
+timestep = 50
+
+# Postprocessing
+dirpost = os.path.join(REP_MUSC,'post')
+variablesDict = 'variables.py'
+defaultConfigPost = 'config_default.py'
+caseDependent = True
+lfaformat = 8
+
+# EMS configuration
+model = 'AROME'
+lsurfex = False
+loverwrite = True
+lupdate_ATM = True 
+lupdate_RUN = True
+
+# ecoclimap data
+ecoclimap = os.path.join(REP_EMS, 'UTIL', 'ecoclimap_cnrm_cm6.02')
+
+############# End editing
--- Examples/config/config_41t1_ARPPNT_OPER.py
+++ Examples/config/config_41t1_ARPPNT_OPER.py
@@ -1,8 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+
 import os
+REP_EMS = os.getenv('REP_EMS')
+REP_MUSC = os.getenv('REP_MUSC')
 
-rep0 = os.getenv('REP_MUSC')
+############# Begin editing
 
-config = 'ARPPNT_OPER'
-cycle = '41t1_op1.11.GFORTRAN610.x.EB.MUSC'
+GROUP = '41t1'
+# EXPID will be taken from this file name following the convention config_EXPID.py (__name__ = config_EXPID) 
+EXPID = __name__[7:]
+
+# Binaries
 MASTER = '/cnrm/amacs/USERS/roehrig/share/MUSC/41t1_op1.11.GFORTRAN610.x.EB.MUSC/bin/MASTERODB'
-namATMref = rep0 + '/namelist/ARPPNT/namarp_41t1_ARPEGE_OPER'
+
+# Namelists
+ATMNAM = os.path.join(REP_MUSC, 'namelist/ARPPNT/namarp_41t1_ARPEGE_OPER')
+
+# Model configuration
+vert_grid = os.path.join(REP_MUSC, 'grid/L105.dta')
+timestep = 300
+
+# Postprocessing
+dirpost = os.path.join(REP_MUSC,'post')
+variablesDict = 'variables.py'
+defaultConfigPost = 'config_default.py'
+caseDependent = True
+lfaformat = 8
+
+# EMS configuration
+model = 'ARPPNT'
+lsurfex = False
+loverwrite = True
+lupdate_ATM = True 
+lupdate_RUN = True
+
+# ecoclimap data
+ecoclimap = os.path.join(REP_EMS, 'UTIL', 'ecoclimap_cnrm_cm6.02')
+
+############# End editing
