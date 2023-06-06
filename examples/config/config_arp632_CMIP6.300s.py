ea75d94e0878e59ae48ae33520a9f2c78f603034
Romain Roehrig <romain.roehrig@gmail.com>
2023-06-06 10:58:09 +0200

    Convergence toward mac branch

---
+++ examples/config/config_arp632_CMIP6.300s.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+
+import os
+REP_MUSC = '__REP_MUSC__'
+
+############# Begin editing
+
+GROUP = 'V632'
+# EXPID will be taken from this file name following the convention config_EXPID.py
+
+# Binaries
+bindir = '/cnrm/amacs/USERS/roehrig/share/EMS/pack/arp632_climat.02.MPIGNU930.cx/bin'
+MASTER = os.path.join(bindir, 'MASTER')
+PGD = os.path.join(bindir, 'PGD')
+PREP = os.path.join(bindir, 'PREP')
+ASCII2FA = os.path.join(bindir, 'ASCII2FA')
+
+# Namelists
+ATMNAM = os.path.join(REP_MUSC, 'namelist/ARPCLIMAT/nam.atm.tl127l91r.CMIP6.v631')
+SFXNAM = os.path.join(REP_MUSC, 'namelist/SURFEX/nam.sfx.tl127.CMIP6.v631')
+
+# Model configuration
+vert_grid = os.path.join(REP_MUSC, 'grid/L91.dta')
+timestep = 300
+
+# Postprocessing
+dirpost = os.path.join(REP_MUSC,'post')
+variablesDict = 'variables.py'
+defaultConfigPost = 'config_default.py'
+caseDependent = True
+
+# EMS configuration
+model = 'ARPCLIMAT'
+#lascii_forc
+lsurfex = True
+#sfxfmt
+loverwrite = True
+lupdate_ATM = True
+lupdate_SFX = True
+lupdate_RUN = True
+
+# ecoclimap data
+#ecoclimap
+
+############# End editing
