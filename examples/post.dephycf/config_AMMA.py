87c14940e53a67d87ff55b2daee3e57f9c369f2e
Romain Roehrig <romain.roehrig@gmail.com>
2025-04-24 15:56:35 +0200

    Update for 49t1 + config output for transition cases

--- examples/post.dephycf/config_AMMA.py
+++ examples/post.dephycf/config_AMMA.py
@@ -7,9 +7,22 @@
 #saveall = True
 saveall = False
 
-var2save = ['pfull','zfull','theta','ta','qv','ua','va','wap','ql','qi','qr','qsn','cl','clt','prw','pr','hfls','hfss','ts','hur','Q1','Q2','qlc','qic','qrc','qsnc','tke','wa_up','mf_up','alpha_up','ustar','z0','dTv_up','ta_up','qv_up','ent_up','det_up','b_up','ent_up_org','ent_up_turb','det_up_org','det_up_turb','lwp','iwp','tnqt','tnthl']
 
-convert2p = True #False
+var2save = ['pfull','zfull']\
+    + ['theta','ta','qv','hur','ua','va','wap','ql','qi','qr','qsn','qlc','qic','qrc','qsnc','tke']\
+    + ['cl','clt','prw','iwp','lwp','rwp','swp','cwp']\
+    + ['iwp_rad','lwp_rad','cwp_rad','iwp_conv','lwp_conv','rwp_conv','swp_conv','cwp_conv']\
+    + ['pr','hfls','hfss','ustar','z0','ts',]\
+    + ['wa_up','mf_up','alpha_up','b_up','dTv_up','ent_up','det_up']\
+    + ['Q1','Q2']\
+    + ['tnta_diab','tnta_turb','tnta_micro','tnta_conv','tnta_radlw','tnta_radsw','tnta_other']\
+    + ['tnta_adv','tnta_nud']\
+    + ['tnqv_diab','tnqv_turb','tnqv_micro','tnqv_conv','tnqv_other']\
+    + ['tnqv_adv','tnqv_nud']\
+    + ['tnua_turb','tnua_conv','tnua_other','tnua_geo']\
+    + ['tnva_turb','tnva_conv','tnva_other','tnva_geo']
+
+convert2p = False
 convert2z = False
 
 convertkday = False
